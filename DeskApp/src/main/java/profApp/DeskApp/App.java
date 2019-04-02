7c25bd415df78a8731d9911e10e89485ff983dc2
kumarkalyan93 <kkalyan3@asu.edu>
2019-04-02 15:59:26 -0700

    Initial layout added to Dev,Please branch for features now

--- DeskApp/src/main/java/profApp/DeskApp/App.java
+++ DeskApp/src/main/java/profApp/DeskApp/App.java
@@ -1,5 +1,11 @@
 package profApp.DeskApp;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import profApp.Utils.QuizConstants;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	JFileChooser jFile =  new JFileChooser();
+    	jFile.setDialogTitle("Select directory");
+    	jFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    	jFile.setAcceptAllFileFilterUsed(false);
+    	
+    	if(jFile.showOpenDialog(null)== JFileChooser.APPROVE_OPTION)
+    	{
+    		File dirName = jFile.getSelectedFile();
+    		QuizConstants.directoryName = dirName.getAbsolutePath();
+    		//System.out.println(QuizConstants.directoryName);
+    	}
+        ApplicationWindow appWindow = new ApplicationWindow();
+       
     }
 }

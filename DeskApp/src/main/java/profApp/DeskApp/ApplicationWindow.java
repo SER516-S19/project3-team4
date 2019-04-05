634f451954bae1259f5af3220d518e6c51e6b32c
HarshitaKAM <hkajal@asu.edu>
2019-04-04 22:14:04 -0700

    US#2 'Edit UI changes'

--- DeskApp/src/main/java/profApp/DeskApp/ApplicationWindow.java
+++ DeskApp/src/main/java/profApp/DeskApp/ApplicationWindow.java
@@ -2,6 +2,8 @@ package profApp.DeskApp;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -9,6 +11,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+import java.awt.Color;
 import java.awt.Font;
 
 public class ApplicationWindow extends JFrame {
@@ -19,7 +24,7 @@ public class ApplicationWindow extends JFrame {
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JButton jButtonCreate = new JButton("Create Quiz");
+		final JButton jButtonCreate = new JButton("Create Quiz");
 		jButtonCreate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -27,15 +32,42 @@ public class ApplicationWindow extends JFrame {
 		jButtonCreate.setBounds(78, 143, 110, 33);
 		panel.add(jButtonCreate);
 		
-		JButton jButtonEdit = new JButton("Edit Quiz");
+		final JButton jButtonEdit = new JButton("Edit Quiz");
+		jButtonEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				editNextFrame editWindow=new editNextFrame();
+				editWindow.setVisible(true);
+			}
+		});
 	
 		jButtonEdit.setBounds(254, 143, 89, 33);
 		panel.add(jButtonEdit);
 		
-		JLabel jLblWelcome = new JLabel("Welcome to the Quiz App");
+		jButtonEdit.addMouseListener(new MouseAdapter() {
+			public void mouseExited(MouseEvent event) {
+		        jButtonEdit.setBackground(UIManager.getColor("control"));
+		    }
+		    public void mouseEntered(MouseEvent evt) {
+		        jButtonEdit.setBackground(Color.GREEN);
+		    }
+		});
+		
+		jButtonCreate.addMouseListener(new MouseAdapter() {
+			public void mouseExited(MouseEvent event) {
+		        jButtonCreate.setBackground(UIManager.getColor("control"));
+		    }
+		    public void mouseEntered(MouseEvent event) {
+		    	jButtonCreate.setBackground(Color.GREEN);
+		    }
+		});
+		
+		JLabel jLblWelcome = new JLabel("Welcome to the Quiz App: Professor View");		
 		jLblWelcome.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 17));
 		jLblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
-		jLblWelcome.setBounds(42, 37, 339, 56);
+		jLblWelcome.setBounds(60, 35, 339, 56);
 		panel.add(jLblWelcome);
+		
 	}
 }
+
+
---
+++ DeskApp/src/main/java/profApp/DeskApp/editNextFrame.java
@@ -0,0 +1,51 @@
+package profApp.DeskApp;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import java.awt.Font;
+
+public class editNextFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					editNextFrame frame = new editNextFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public editNextFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Please select a quiz to edit:");
+		lblNewLabel.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+	}
+
+}

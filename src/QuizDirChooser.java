d9a3b0ab19b535918f32f95bd506065da777ba28
HongfeiJu <hongfeiju@gmail.com>
2019-04-10 14:56:14 -0700

    add controller

--- src/QuizDirChooser.java
+++ src/QuizDirChooser.java
@@ -1,10 +1,18 @@
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
+/**
+ * A directory chooser UI that allow the user to select the folder containing quiz files
+ * @author Hongfei Ju
+ * @version 1.0
+ */
 public class QuizDirChooser {
 	private JFileChooser chooser = null;
 	private String path = null;
-	public QuizDirChooser(JFrame frame) {		
+	private JFrame frame = null;
+	
+	public QuizDirChooser() {
+		frame = new JFrame();
 		chooser = new JFileChooser(); 
 	    chooser.setCurrentDirectory(new java.io.File("."));
 	    chooser.setDialogTitle("select quizzes");

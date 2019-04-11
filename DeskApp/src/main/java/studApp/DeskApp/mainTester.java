c4cda5ba56cece327d47109d366c26e212ca3d52
amankaushik <amankaushik104@gmail.com>
2019-04-11 15:44:05 -0700

    US#10 Fix filename extraction from path

--- DeskApp/src/main/java/studApp/DeskApp/mainTester.java
+++ DeskApp/src/main/java/studApp/DeskApp/mainTester.java
@@ -4,31 +4,30 @@ package studApp.DeskApp;
 
 import java.awt.EventQueue;
 import java.util.List;
-
 import studApp.DeskApp.dao.QuizDAO;
 import studApp.DeskApp.operations.JSONParser;
 
 /**
- * @author amankaushik 
+ * @author amankaushik
  * @author paulhorton
  */
 public class mainTester {
 
-    public static void main(String[] args) {
-        QuizDAO quizDAO = JSONParser.parseFile("src/main/java/studApp/DeskApp/check.json", null);
-        System.out.println(quizDAO.getQuizName());
-        System.out.println(quizDAO.getQuestionList());
-        
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					QuestionController qc = new QuestionController(quizDAO);
-					QuestionWindow window = new QuestionWindow(qc);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-    }
-    
+  public static void main(String[] args) {
+    List<QuizDAO> quizDAO = JSONParser.parseDirectory("/home/amankaushik/project3-team4/Quizzes/", null);
+    System.out.println(quizDAO.get(0).getQuizName());
+    System.out.println(quizDAO.get(0).getQuestionList());
+
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          QuestionController qc = new QuestionController(quizDAO.get(0));
+          QuestionWindow window = new QuestionWindow(qc);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
 }

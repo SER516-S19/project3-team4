9b9cfc015f410832dfc842577a76a5ebe43a0ad1
kumarkalyan93 <kkalyan3@asu.edu>
2019-04-12 18:50:58 -0700

    Cleaning code

--- DeskApp/src/main/java/profApp/DeskApp/Views/EditQuestionWindow.java
+++ DeskApp/src/main/java/profApp/DeskApp/Views/EditQuestionWindow.java
@@ -42,12 +42,42 @@ public class EditQuestionWindow extends JFrame implements ActionListener {
 		this.setBounds(300, 300, 600, 600);
 		this.setVisible(true);
 		mapChoices = new HashMap<String, String>();
+		setFields();
+		addOptionFields();
+		setCorrectAnswerChoices(question);
+		setLabels();
+		addButtons();
 
-		JLabel lblNewLabel = new JLabel("Question " + (index + 1));
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		lblNewLabel.setBounds(31, 11, 76, 14);
-		getContentPane().add(lblNewLabel);
+	}
 
+	private void setCorrectAnswerChoices(Question question) {
+		String choices[] = null;
+		correctChoice = new JComboBox<String>();
+		if (question != null) {
+			for (int i = 0; i < QuizConstants.options.length; i++) {
+				if (question.getOptions() != null && question.getOptions().size() > 0)
+					mapChoices.put(QuizConstants.options[i], question.getOptions().get(i));
+				correctChoice.addItem(QuizConstants.options[i]);
+			}
+			if (question.getOptions() != null && question.getOptions().size() > 0) {
+				option1.setText(question.getOptions().get(0));
+				option2.setText(question.getOptions().get(1));
+				option3.setText(question.getOptions().get(2));
+				option4.setText(question.getOptions().get(3));
+				System.out.println(question.getOptions().indexOf(question.getCorrectAnswer()));
+				correctChoice.setSelectedItem(
+						QuizConstants.options[question.getOptions().indexOf(question.getCorrectAnswer())]);
+			}
+			questionTitle.setText(question.getTitle());
+			choices = QuizConstants.options;
+		}
+
+		correctChoice.setBounds(31, 389, 348, 20);
+		getContentPane().add(correctChoice);
+		correctChoice.setEditable(true);
+	}
+
+	private void setFields() {
 		questionTitle = new JFormattedTextField();
 		questionTitle.setBounds(31, 36, 348, 58);
 		getContentPane().add(questionTitle);
@@ -72,62 +102,26 @@ public class EditQuestionWindow extends JFrame implements ActionListener {
 		option4.setBounds(31, 310, 348, 20);
 		getContentPane().add(option4);
 		option4.setEditable(true);
-
-		String choices[] = null;
-
-		addOptionFields();
-		
-		correctChoice = new JComboBox<String>();
-		if (question != null) {
-			for (int i = 0; i < QuizConstants.options.length; i++) {
-				if (question.getOptions() != null && question.getOptions().size() > 0)
-					mapChoices.put(QuizConstants.options[i], question.getOptions().get(i));
-				correctChoice.addItem(QuizConstants.options[i]);
-			}
-			if (question.getOptions() != null && question.getOptions().size() > 0) {
-				option1.setText(question.getOptions().get(0));
-				option2.setText(question.getOptions().get(1));
-				option3.setText(question.getOptions().get(2));
-				option4.setText(question.getOptions().get(3));
-				System.out.println(question.getOptions().indexOf(question.getCorrectAnswer()));
-				correctChoice.setSelectedItem(
-						QuizConstants.options[question.getOptions().indexOf(question.getCorrectAnswer())]);
-			}
-			questionTitle.setText(question.getTitle());
-			choices = QuizConstants.options;
-		}
-
-		correctChoice.setBounds(31, 389, 348, 20);
-		getContentPane().add(correctChoice);
-		correctChoice.setEditable(true);
-		setOptionLabels();
-
-		JLabel lblCorrectAnswer = new JLabel("Correct Answer:");
-		lblCorrectAnswer.setBounds(31, 364, 97, 14);
-		getContentPane().add(lblCorrectAnswer);
-
-		addButtons();
-
 	}
 
 	private void addButtons() {
-		btnNext = new JButton("Next");
+		btnNext = new JButton(QuizConstants.BUTTON_LABEL_NEXT);
 		btnNext.setBounds(31, 436, 89, 23);
 		btnNext.addActionListener(this);
 		getContentPane().add(btnNext);
 
-		btnBack = new JButton("Back");
+		btnBack = new JButton(QuizConstants.BUTTON_LABEL_BACK);
 		btnBack.setBounds(153, 436, 89, 23);
 		btnBack.addActionListener(this);
 		getContentPane().add(btnBack);
 
-		btnCancel = new JButton("Cancel");
+		btnCancel = new JButton(QuizConstants.BUTTON_LABEL_CANCEL);
 		btnCancel.setBounds(276, 436, 89, 23);
 		btnCancel.addActionListener(this);
 		getContentPane().add(btnCancel);
 
-		buttonRemove = new JButton("Remove this Question ?");
-		buttonRemove.setBounds(437, 177, 111, 23);
+		buttonRemove = new JButton(QuizConstants.BUTTON_LABEL_REMOVE);
+		buttonRemove.setBounds(416, 177, 145, 23);
 		buttonRemove.addActionListener(this);
 		getContentPane().add(buttonRemove);
 	}
@@ -137,26 +131,24 @@ public class EditQuestionWindow extends JFrame implements ActionListener {
 		option1.setBounds(31, 154, 348, 20);
 		getContentPane().add(option1);
 		option1.setEditable(true);
-		
 
 		option2 = new JFormattedTextField();
 		option2.setBounds(31, 206, 348, 20);
 		getContentPane().add(option2);
 		option2.setEditable(true);
-		
 
 		option3 = new JFormattedTextField();
 		option3.setBounds(31, 256, 348, 20);
 		getContentPane().add(option3);
 		option3.setEditable(true);
-		
+
 		option4 = new JFormattedTextField();
 		option4.setBounds(31, 310, 348, 20);
 		getContentPane().add(option4);
 		option4.setEditable(true);
 	}
 
-	private void setOptionLabels() {
+	private void setLabels() {
 		JLabel lblOption = new JLabel("Option 1");
 		lblOption.setBounds(31, 129, 76, 14);
 		getContentPane().add(lblOption);
@@ -172,16 +164,24 @@ public class EditQuestionWindow extends JFrame implements ActionListener {
 		JLabel lblOption_3 = new JLabel("Option 4");
 		lblOption_3.setBounds(31, 285, 76, 14);
 		getContentPane().add(lblOption_3);
+
+		JLabel lblNewLabel = new JLabel("Question " + (index + 1));
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblNewLabel.setBounds(31, 11, 76, 14);
+		getContentPane().add(lblNewLabel);
+
+		JLabel lblCorrectAnswer = new JLabel(QuizConstants.LABEL_CORRECT_ANSWER);
+		lblCorrectAnswer.setBounds(31, 364, 97, 14);
+		getContentPane().add(lblCorrectAnswer);
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource().equals(btnNext)) {
-			if (e.getActionCommand().equalsIgnoreCase("Next")) {
+			if (e.getActionCommand().equalsIgnoreCase(QuizConstants.BUTTON_LABEL_NEXT)) {
 
 				question.setTitle(questionTitle.getText());
 				if (question.getOptions() == null)
 					question.setOptionsList();
-                    								
 
 				setQuestion();
 
@@ -193,38 +193,25 @@ public class EditQuestionWindow extends JFrame implements ActionListener {
 
 		}
 		if (e.getSource().equals(btnBack)) {
-			if (e.getActionCommand().equalsIgnoreCase("Back")) {
+			if (e.getActionCommand().equalsIgnoreCase(QuizConstants.BUTTON_LABEL_BACK)) {
 
-				if(index>0) {
-				question.setTitle(questionTitle.getText());
-				question.getOptions().set(0, option1.getText());
-				question.getOptions().set(1, option2.getText());
-				question.getOptions().set(2, option3.getText());
-				question.getOptions().set(3, option4.getText());
-				question.setCorrectAnswer((String) correctChoice.getSelectedItem());
-				String splitter[] = ((String) correctChoice.getSelectedItem()).split(" ");
-				question.setCorrectAnswer(question.getOptions().get(Integer.parseInt(splitter[1])));
-				editQuiz.setQuestion(index, question);
-				editQuiz.setIndex(index - 1);
-				editQuiz.startEdit();
-
-				if (e.getActionCommand().equalsIgnoreCase("Back")) {
-                    if(index>0) {
+				if (index > 0) {
 					setQuestion();
 					editQuiz.setQuestion(index, question);
 					editQuiz.setIndex(index - 1);
 					editQuiz.startEdit();
-                    }
+
 				}
 			}
 
 		}
 		if (e.getSource().equals(btnCancel)) {
-			this.dispose();
+			if (e.getActionCommand().equalsIgnoreCase(QuizConstants.BUTTON_LABEL_CANCEL))
+				this.dispose();
 
 		}
 		if (e.getSource().equals(buttonRemove)) {
-			if (e.getActionCommand().equalsIgnoreCase("Remove this Question ?")) {
+			if (e.getActionCommand().equalsIgnoreCase(QuizConstants.BUTTON_LABEL_REMOVE)) {
 				System.out.println("removing");
 				this.dispose();
 				if (editQuiz.remove(question)) {
@@ -237,14 +224,13 @@ public class EditQuestionWindow extends JFrame implements ActionListener {
 		}
 
 	}
-}
 
 	private void setQuestion() {
-		question.getOptions().set(0,option1.getText());
+		question.getOptions().set(0, option1.getText());
 		question.getOptions().set(1, option2.getText());
 		question.getOptions().set(2, option3.getText());
 		question.getOptions().set(3, option4.getText());
-		String splitter[] = ((String)correctChoice.getSelectedItem()).split(" ");
-		question.setCorrectAnswer(question.getOptions().get(Integer.parseInt(splitter[1])-1));
+		String splitter[] = ((String) correctChoice.getSelectedItem()).split(" ");
+		question.setCorrectAnswer(question.getOptions().get(Integer.parseInt(splitter[1]) - 1));
 	}
 }

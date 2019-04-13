5a463b3c062835161c7aee35f90b5e050d2c0802
truptikhatavkar <43074508+truptikhatavkar@users.noreply.github.com>
2019-04-12 18:09:23 -0700

    Clean code

--- DeskApp/src/main/java/profApp/model/Quiz.java
+++ DeskApp/src/main/java/profApp/model/Quiz.java
@@ -3,24 +3,20 @@ package profApp.model;
 import java.util.ArrayList;
 
 /**
- * Quiz class
- *
+ * This class consists the initialization of questions list.
  * @author akashkadam
  * @version 1.0
  */
 public class Quiz {
-
 	private ArrayList<Question> questions;
 
 	/**
 	 * Parameterized constructor with questionList attribute.
-	 *
 	 */
 	public Quiz(ArrayList<Question> questionList) {
 		super();
 		this.questions = questionList;
 	}
-
 	public ArrayList<Question> getQuestionList() {
 		return questions;
 	}

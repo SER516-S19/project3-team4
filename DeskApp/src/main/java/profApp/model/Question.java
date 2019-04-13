bb34130e62270baf6bf30c25c15e393ffe83dc25
truptikhatavkar <43074508+truptikhatavkar@users.noreply.github.com>
2019-04-12 18:12:12 -0700

    Clean code Question.java

--- DeskApp/src/main/java/profApp/model/Question.java
+++ DeskApp/src/main/java/profApp/model/Question.java
@@ -4,63 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Quiz class
- *
+ * This class consists initialization of question title, options and correct 
+ * answer
  * @author akashkadam
  * @version 1.0
  */
 public class Question {
-
 	private String title;
-	
 	private List<String> options;
-	
 	private String correctAnswer;
 
-	 /**
-     * @param String : get title of Question
-     */
+	/**
+    	 * @param String : get title of Question
+    	 */
 	public String getTitle() {
 		return title;
 	}
-
 	 /**
-     * @param title : set title of Question
-     */
+     	* @param title : set title of Question
+    	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
 	 /**
-     * @param List : get options of Question
-     */
+    	 * @param List : get options of Question
+    	 */
 	public List<String> getOptions() {
 		return options;
 	}
-
 	 /**
-     * @param options : set options of Question
-     */
+    	 * @param options : set options of Question
+   	 */
 	public void setOptions(String option) {
 		if(this.options == null) {
 			this.options = new ArrayList<String>();
 		}
 		this.options.add(option);
 	}
-
 	 /**
-     * @param String : get correct answer of Question
-     */
+     	* @param String : get correct answer of Question
+     	*/
 	public String getCorrectAnswer() {
 		return correctAnswer;
 	}
-
 	 /**
-     * @param correct answer : set correct answer of Question
-     */
+     	* @param correct answer : set correct answer of Question
+     	*/
 	public void setCorrectAnswer(String correctAnswer) {
 		this.correctAnswer = correctAnswer;
 	}
-	
-	
 }
